50c1ff6b515f8b508471110778cb7568405a14d5
Cap Bao Dien <diencb@innotech-vn.com>
2023-09-21 18:02:05 +0700

    feat: layout for dashboard

---
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -0,0 +1,7 @@
+export default function CoursesPage() {
+  return (
+    <div>
+      <h1>CoursePage</h1>
+    </div>
+  )
+}
---
+++ app/(dashboard)/components/index.ts
@@ -0,0 +1,5 @@
+export * from './logo'
+export * from './sidebar-item'
+export * from './sidebar-routes'
+export * from './sidebar'
+export * from './navbar'
